14c13a68020006e9e9548277041a4a7e24bcf712
a1778478 <56853075+Crazyorchid@users.noreply.github.com>
2020-09-18 08:17:04 +0000

    trial

---
+++ assignment1/TicketBooker.h
@@ -0,0 +1,14 @@
+//
+// Created by runtao on 17/9/20.
+//
+
+#ifndef ASSIGNMENT1_TICKETBOOKER_H
+#define ASSIGNMENT1_TICKETBOOKER_H
+
+
+class TicketBooker {
+
+};
+
+
+#endif //ASSIGNMENT1_TICKETBOOKER_H
